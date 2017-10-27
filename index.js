b80dc93a11ba7228a7e02a53c3aca71d968a9a61
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-27 13:43:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function postComment() {
-  var commenter;
-  var comment;
+  var commenter = document.querySelectorAll("[type='text']");
+  var comment = document.querySelectorAll("[type='textfield']");
 }
