874532f8a7caedc8db91b2efed61bfe873cec108
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-27 14:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function postComment() {
   var commenter = document.getElementById('commenter').value;
   var comment = document.getElementById('comment').value;
 
-  var commentTemplate = document.getElementById('commentTemplate');;
+  var commentTemplate = document.getElementById('comment-template');;
   var templateFn = _.template(commentTemplate);
 
   var commentsDiv = document.getElementById('comments');
